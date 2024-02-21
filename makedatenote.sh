98284e78fe64eabc81985d6970fb45e8fb199e2e
vayers <violetlastname@gmail.com>
2024-02-21 08:32:25 -0500

    add comments to describe what the scripts do

--- makedatenote.sh
+++ makedatenote.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 
+# Opens a markdown file in nvim using the days date
+# date is formatted as year-month-day
+
 nvim $(date +%Y-%m-%d).md
--- snip.sh
+++ snip.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+#
+# take a screenshot of a portion of the screen
+# basically snipping tool
+
 grim -g "$(slurp)"
